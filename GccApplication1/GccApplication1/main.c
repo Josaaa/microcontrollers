1b786605119cdac29965eee57b870d0f8e22d6e3
Joëlle <joelle.a.m.devries@gmail.com>
2020-01-29 12:43:12 +0100

    1

---
+++ GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 29/01/2020 12:42:50
+ * Author : joelle
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
