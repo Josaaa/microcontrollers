06a342a0bba4cc83f6df63538f44ad673942e1f1
Joëlle <joelle.a.m.devries@gmail.com>
2020-01-30 09:50:17 +0100

    n

--- GccApplication1/GccApplication1/main.c
+++ GccApplication1/GccApplication1/main.c
@@ -4,15 +4,58 @@
  * Created: 29/01/2020 12:42:50
  * Author : joelle
  */ 
+#define F_CP 8e0
 
 #include <avr/io.h>
+#include <util/delay.h>
 
+void wait(int miliseconds)
+{
+	for (int i = 0; i< miliseconds; i++)
+	{
+		
+		_delay_ms ( 1 );
+	}
+
+}
 
 int main(void)
 {
     /* Replace with your application code */
-    while (1) 
-    {
-    }
+	
+	
+}
+
+void omenom(){
+	DDRD = 0b11111111;			// All pins PORTD are set to output
+	DDRC = 0xff;
+	while (1)
+	{
+		//PORTD = 0xAA;			// Write 10101010b PORTD
+		PORTC = 0b10000000;
+		
+		wait( 500 );
+		//PORTD = 0x55;			// Write 01010101b PORTD
+		PORTC = 0b01000000;
+		wait( 500 );
+	}
+
+
+}
+
+void eerste(){
+	 /*Replace with your application code */
+	 
+	 DDRD = 0b11111111;
+	 while (1)
+	 {
+	 PORTD = 0xAA;
+	 wait( 250 );
+	 PORTD = 0x55;			// Write 01010101b PORTD
+	 wait( 250 );
+	 }
+	 
+	 return 1;
+
 }
 
